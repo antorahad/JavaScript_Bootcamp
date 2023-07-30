ea0071106fe89e4c0834ef21a2bdf31b73f99aec
antorahad <113632611+antorahad@users.noreply.github.com>
2023-07-31 00:28:08 +0600

    condition condition condition mama koto condition

---
+++ condition.js
@@ -0,0 +1,35 @@
+// if else condition
+
+var price = 700;
+
+var budget = 400;
+
+if(price == budget){
+    console.log('Buy');
+}
+
+else{
+    console.log('Go Home');
+}
+
+
+// Multiple condition
+
+var productPrice = 70000;
+
+var salary = 60000;
+
+var savings = 20000;
+
+
+if(productPrice <= salary || (salary + savings) >= productPrice && savings >= 30000){
+    console.log('I will buy the product');
+}
+
+else{
+    console.log('Budget is not enough');
+}
+
+
+//Multistage condition
+
