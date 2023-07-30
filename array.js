6d634d11c73fbff294bfcdfaa3d4eb7b3d856e55
antorahad <113632611+antorahad@users.noreply.github.com>
2023-07-30 22:58:49 +0600

    Pop push

--- array.js
+++ array.js
@@ -29,3 +29,71 @@ console.log(car.indexOf('Tata'));
 console.log(myGirlFriend.indexOf('Monisha'));
 
 console.log(friendAge.indexOf(25));
+
+
+// Array push
+
+var numbers = [78, 45, 98, 45]
+
+numbers.push(100);
+
+console.log(numbers);
+
+var fruits = ['Mango', 'Banana', 'Orange'];
+
+fruits.push('Dragon fruit');
+
+fruits.push('Jackfruit')
+
+console.log(fruits);
+
+
+//Array pop
+
+var item = [1, 2, 3, 4, 5, 6];
+
+item.pop();
+
+item.push(7);
+
+item.pop()
+
+var element = item.pop();
+console.log(element);
+
+
+console.log(item);
+
+
+var persons = ['Kabir', 'Safiq', 'Rakib', 'Jamil', 'Sadia'];
+
+persons.unshift('Jakir');
+
+console.log(persons);
+
+
+var pet = ['Dog', 'Cat', 'Bird', 'Mouse'];
+
+pet.shift();
+
+pet.shift();
+
+pet.unshift('Spider')
+
+pet.unshift('Bat');
+
+console.log(pet);
+
+var superHero = ['Batman', 'Superman', 'Flash', 'Wonder Woman'];
+
+superHero.push('Cyborg');
+
+superHero.shift();
+
+superHero.unshift('Green lantern');
+
+superHero.pop();
+
+superHero.pop();
+
+console.log(superHero);
