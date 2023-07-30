5efa1ffa69835d29e47e391e42dcb3360c054293
antorahad <113632611+antorahad@users.noreply.github.com>
2023-07-30 20:17:38 +0600

    javascript practices

---
+++ array.js
@@ -0,0 +1,25 @@
+var friendAge = [20, 22, 25, 26, 27];
+
+console.log(typeof friendAge);
+
+var myGirlFriend = ['Sohana', 'Ahona', 'Anona', 'Monisha', 'Munni'];
+
+console.log(myGirlFriend);
+
+
+console.log(myGirlFriend.length)
+
+
+
+// Index of arrays
+
+var car = ['Bmw', 'Ford', 'Toyata', 'Mazda', 'Tata'];
+
+
+//get array from index
+console.log(car[0]);
+
+//set array in index
+car[2] = 'Mr.Bean er gari';
+
+console.log(car);
---
+++ var_recap.js
@@ -0,0 +1,77 @@
+console.log(sum);
+
+var a = 10;
+
+var b = 20;
+
+var sum = a + b;
+
+// Result will be undefined
+
+
+var c = 10;
+
+var d = 20;
+
+var sum2 = c + d ;
+
+console.log(sum2);
+
+// Result will be 30
+
+
+//int var
+
+var sunGlassPrice = 1200;
+
+console.log(typeof sunGlassPrice);
+
+// string var
+
+var actorName = 'Johnny Dep';
+
+console.log(typeof actorName);
+
+// bool var
+
+var areYouSingle = true;
+
+console.log(typeof areYouSingle);
+
+
+// Operation
+
+var a = 10;
+
+var b = 20;
+
+console.log(a+b);
+console.log(b-a);
+console.log(a*b);
+console.log(b/a);
+
+console.log(a += 5);
+
+console.log(b -= 2);
+
+
+
+var x = 3;
+
+var y = x++;
+
+console.log(x);
+
+var int1 = 5;
+
+var int2 = int1++;
+
+console.log(int1);
+
+
+
+var m = 10;
+
+var n = m--;
+
+console.log(`m: ${m} and n: ${n}`);
