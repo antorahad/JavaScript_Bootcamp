50d39673bfb48e45e5bee449ef15bc81418c1cb2
antorahad <113632611+antorahad@users.noreply.github.com>
2023-07-30 23:40:09 +0600

    Compare"

---
+++ comparison.js
@@ -0,0 +1,45 @@
+// <
+
+var a = 15;
+
+var b = 12;
+
+console.log(b < a);
+
+
+// > 
+
+var c = 20;
+
+var d = 15;
+
+console.log(c > d);
+
+// ==
+
+var e = 10 ;
+
+var f = 10 ;
+
+console.log(e == f);
+
+//  !=
+
+console.log(10 != 20);
+
+
+// <=
+
+console.log(4 <= 6);
+
+// >=
+
+console.log(6 >= 4);
+
+// &&
+
+console.log(5>=4 && 4<=5 && 4==4);
+
+// ||
+
+console.log (5 != 5 || 5 > 4)
