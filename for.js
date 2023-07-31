91bcd25c8469f8689df47b3e584d8dfeac3c54ef
antorahad <113632611+antorahad@users.noreply.github.com>
2023-08-01 00:38:44 +0600

    for while

---
+++ for.js
@@ -0,0 +1,13 @@
+// for loop
+for(var even = 0; even <= 10; even+=2){
+    console.log(even);
+}
+
+
+//While loop
+var odd = 1;
+
+while(odd < 10){
+    console.log(odd);
+    odd+=2;
+}
--- loops.js
+++ loops.js
@@ -31,3 +31,19 @@ while(num2 <= 10){
     console.log(num2);
     num2 = num2 + 2
 }
+
+
+// for loop
+
+for(var roastGiven = 0; roastGiven < 7; roastGiven++){
+    console.log('Ekhane roast lagbe');
+}
+
+
+for(var i = 0; i < 7; i++){
+    console.log(i);
+}
+
+for(var odd = 1; odd <= 10; odd = odd+=2){
+    console.log(odd);
+}
