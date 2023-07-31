7a4ee75889c923ea62f75ef9e05c96082e2ea1d0
antorahad <113632611+antorahad@users.noreply.github.com>
2023-08-01 00:07:09 +0600

    loops

---
+++ loops.js
@@ -0,0 +1,33 @@
+// while loop
+
+/*var roastGiven = 0;
+
+while(roastGiven < 7){
+    console.log('Roast de bandirpola!!!!');
+    console.log(roastGiven);
+    roastGiven++;
+}*/
+
+
+/*var number = 0;
+
+while(number <= 20){
+    console.log(number);
+    number++;
+}*/
+
+
+//odd numbers even numbers
+var num = 0;
+
+while(num <= 10){
+    console.log(num);
+    num = num + 2;
+}
+
+var num2 = 1;
+
+while(num2 <= 10){
+    console.log(num2);
+    num2 = num2 + 2
+}
