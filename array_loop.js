5d7a72c23dfbe4a8af549db1a72d5711968dd0ef
antorahad <113632611+antorahad@users.noreply.github.com>
2023-08-01 18:03:02 +0600

    hui hui hui

---
+++ array_loop.js
@@ -0,0 +1,14 @@
+var numbers = [1, 2, 5, 8, 9, 7];
+
+for (var i = 0; i < numbers.length; i++){
+    var number = numbers[i];
+    console.log(number);
+}
+
+// This how you print the whole array using for loop
+var cheeks = ['Ahona', 'Sohana', 'Sabana', 'Farjana', 'Vuski Munni', 'Matari Munni'];
+
+for(var i = 0; i < cheeks.length; i++){
+    var cheek = cheeks[i];
+    console.log(cheek);
+}
