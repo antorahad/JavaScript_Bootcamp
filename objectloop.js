53594d3136434049e2d75ddc5b525a43b706ea26
antorahad <113632611+antorahad@users.noreply.github.com>
2023-08-03 00:04:03 +0600

    object looping

---
+++ objectloop.js
@@ -0,0 +1,35 @@
+var friendAge = {
+    Karim: 20,
+    jamy: 23,
+    lamiya: 18,
+    Ashik: 24,
+    Jakir: 22
+}
+
+// object property
+const Keys = Object.keys(friendAge);
+
+console.log(Keys);
+
+//object value
+const Key2 = Object.values(friendAge);
+
+console.log(Key2);
+
+
+//How to find property and value using for loop of an object
+for(var i=0; i < Keys.length; i++){
+    var propertyName = Keys[i];
+
+    var propertyValue = friendAge[propertyName];
+
+    console.log(propertyName, propertyValue);
+}
+
+
+//East Method for in loop
+
+for(var propertyName in friendAge){
+    var objectValue = friendAge[propertyName];
+    console.log(propertyName, objectValue);
+}
