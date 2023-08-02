8937c5bcb942fc198cd107908fab69bc3ceb4d70
antorahad <113632611+antorahad@users.noreply.github.com>
2023-08-02 14:51:03 +0600

    return in function

---
+++ return.js
@@ -0,0 +1,30 @@
+// Return in function
+
+function gunin(a, b){
+    var mul = a * b ;
+    return mul;
+}
+
+var total = gunin(100, 200);
+
+
+console.log('Gunin Boleche:' + ' ' + total);
+
+
+function intro(name){
+    var introduction = 'Hello myself ' + '' + name;
+    return introduction;
+}
+
+var sttr = intro('Ahad');
+
+console.log(sttr);
+
+var sttr2 = intro('Fahim');
+
+console.log(sttr2);
+
+var sttr3 = intro('JoyBoi');
+
+console.log(sttr3);
+
